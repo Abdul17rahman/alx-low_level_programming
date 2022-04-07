c8ca652838f6badde60d1fefba65d25db6dd66b6
AbdulRahman <abdulnsamba@gmail.com>
2022-04-07 13:03:51 -0700

    Print using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * main: This is the program's entry point where execution starts.
+ *
+ * Return: (0) for success
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
