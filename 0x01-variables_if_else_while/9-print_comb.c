1da5bfd5ee3ec05dfcf98fdea4a4fd9c80f05057
AbdulRahman <abdulnsamba@gmail.com>
2022-04-08 15:21:19 -0700

    Single digits

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,12 +14,12 @@
 int main(void)
 {
 	int num;
-	char let;
 
 	for (num = '0'; num <= '9'; num++)
 		putchar(num);
-		putchar(',');
-		putchar(' ');
+		if (num != '9')
+			putchar(',');
+			putchar(' ');
 	putchar('\n');
 	return (0);
 }
