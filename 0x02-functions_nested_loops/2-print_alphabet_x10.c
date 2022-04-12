db1220c0c9030b2c04ad78fbdd6003e891f0d2f9
AbdulRahman <abdulnsamba@gmail.com>
2022-04-12 13:24:54 -0700

    Print alha 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+
+#include "main.h"
+
+/**
+* print_alphabet - description check
+*
+* Descriptions: It print the alphabet in lowercase follow by newline
+*
+* Returns: nothing
+*/
+
+void print_alphabet(void)
+
+{
+	char alpha;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+}
