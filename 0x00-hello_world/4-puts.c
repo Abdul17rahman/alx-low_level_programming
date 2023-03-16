e4e613ac55c4c634da4a36a2a74375b75b67390a
Abdul Rahman <abdulnsamba@gmail.com>
2023-03-16 12:23:57 +0000

    Print using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - uses puts to print a statement.
+ * Description: Pass the string to Puts function
+ *
+ * Return: main returns 0 for true
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
