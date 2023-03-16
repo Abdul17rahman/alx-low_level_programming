63e6a2944e2ec394f5f5d5621fb1b291f5d50747
Abdul Rahman <abdulnsamba@gmail.com>
2023-03-16 13:01:56 +0000

    Print using Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,10 +4,10 @@
  * main - prints a string using printf
  * Description: This functions prints a string passed into printf
  * Return: main return 0 for true
- * */
+ */
 
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammer, but the outcome is a piece of art,");
 	return (0);
 }
