903a68189d63a746d71b65dcaaa53ec618e9c13f
Abdul Rahman <abdulnsamba@gmail.com>
2023-03-16 14:41:33 +0000

    Print to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - program prints output to standard error
+ * Description: Takes in arguments and prints an error
+ * Return: main return 1 as an error 
+*/
+int main(void)
+{
+	fprintf(stderr,"%s","and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
