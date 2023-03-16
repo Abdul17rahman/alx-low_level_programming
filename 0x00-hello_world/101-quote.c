21cb4df6bdd5f8dc7af3d972db0a7e4e17075713
Abdul Rahman <abdulnsamba@gmail.com>
2023-03-16 15:23:12 +0000

    Updated print with printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - program prints output to standard error
  * Description: Takes in arguments and prints an error
- * Return: main return 1 as an error 
+ * Return: main return 1 as an error
 */
 int main(void)
 {
-	fprintf(stderr,"%s","and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
