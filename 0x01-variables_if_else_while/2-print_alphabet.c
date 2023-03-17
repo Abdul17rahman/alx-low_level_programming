60aedcb9e9dc2abfb7250266d2f1cc4694826ff4
Abdul Rahman <abdulnsamba@gmail.com>
2023-03-18 00:45:54 +0300

    Print alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints lowcase chars
+ * Description: Uses a for loop
+ * Return: main return 0 for success
+ */
+
+int main(void)
+{
+	for (int a = 97; a <= 122; a++)
+		putchar(a);
+}
