06bdcc5efaf3178c3d55a097f2edb01c6dd77ca2
Abdul Rahman <abdulnsamba@gmail.com>
2023-03-18 01:50:34 +0300

    Alpha in Rev

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints lowcase chars in reverse
+ * Description: Uses a for loop
+ * Return: main return 0 for success
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 122; a >= 97; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
