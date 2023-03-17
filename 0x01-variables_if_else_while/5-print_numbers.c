8c259b0e95227307c704ba900faa3a6b901a5d44
Abdul Rahman <abdulnsamba@gmail.com>
2023-03-18 01:26:36 +0300

    Base 10 digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints single digits of base 10
+ * Description: Uses a for loop
+ * Return: main return 0 for success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
