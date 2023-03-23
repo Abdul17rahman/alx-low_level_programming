29ad49c3ff2d39ae58894fd4d22c5b981139db4d
Abdul Rahman <abdulnsamba@gmail.com>
2023-03-23 11:04:01 +0300

    isUppercase

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _putchar - prints a char using putchar
+ * @c: c is the character that is printed
+ *
+ * Description: This functions prints a char
+ * Return: main return 0 for true
+ */
+
+int _putchar(char c)
+{
+	putchar(c);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef header_file
+#define header_file
+
+#include <stdio.h>
+
+int _isupper(int c);
+
+#endif
