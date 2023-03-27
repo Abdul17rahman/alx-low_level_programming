583c63caca48eac5f1ce7ba5751fed5359242d87
Abdul Rahman <abdulnsamba@gmail.com>
2023-03-27 18:17:26 +0300

    Reset Int

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - Reset a value to 98
- * @*n: n is a pointer/address of the value we're changing
+ * @n: n is a pointer/address of the value we're changing
  *
  * Description: This function takes a pointer as an input
  * Return: 0 for success
