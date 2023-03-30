0ed9ca6463f50fdab7698ca85bb48377c579d5c3
Abdul Rahman <abdulnsamba@gmail.com>
2023-03-31 00:15:24 +0300

    Strncopy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies a string
+ * @dest: The destination variable
+ * @src: Source pointer
+ * @n: the number of bytes
+ *
+ * Description: Uses a for loop to copy strings the string
+ * Return: a pointer to the string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int p = strlen(src);
+	int k, l;
+
+	k = 0;
+
+	for (l = 0; l < p && l < n; l++)
+	{
+		dest[k] = src[l];
+		k++;
+	}
+	return (dest);
+}
