dd1b8e3eeee8a81037cb6b7a6ebbebb05eccdc6f
Abdul Rahman <abdulnsamba@gmail.com>
2023-03-30 17:17:19 +0300

    Conc

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates a string
+ * @dest: The string to print
+ * @src: Source of the file
+ *
+ * Description: Uses a for loop to concatenate the string
+ * Return: a pointer to the string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *new;
+
+	while (src[len] != '\0')
+	{
+		_putchar(str[len]);
+		len++;
+	}
+	return (new);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef header_file
+#define header_file
+
+#include <stdio.h>
+#include <string.h>
+
+char *_strcat(char *dest, char *src);
+
+#endif
