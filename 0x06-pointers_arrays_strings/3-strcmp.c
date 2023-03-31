14a09e2efe1985b3800edf35ede43c4dcbc2892a
Abdul Rahman <abdulnsamba@gmail.com>
2023-03-31 15:13:22 +0300

    Comprae

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -19,17 +19,17 @@ int _strcmp(char *s1, char *s2)
 	{
 		for (k = 0; k < n; k++)
 		{
-			if (s1[j] == s2[k])
-			{
-				return (0);
-			}
-			else if (s1[j] < s2[k])
+			if (s1[j] < s2[k])
 			{
 				return (-15);
 			}
-			else
+			else if (s1[j] > s2[k])
 			{
 				return (15);
+			}
+			else
+			{
+				return (0);
 			}
 		}
 	}
