9ff955bda1d893b7d3b67837e7b948f6aa0ce492
Abdul Rahman <abdulnsamba@gmail.com>
2023-04-03 16:50:25 +0300

    mem

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,27 +1,22 @@
 #include "main.h"
 
 /**
- * _memset - Fills memory
- * @s: Pointer to the memory area
- * @b: constant byte
+ * _memcpy - Copies memory
+ * @dest: Pointer to destination memory
+ * @src: Pointer to source memory
  * @n: Numbers of bytes
  *
- * Description: This functions fills a memory area with b.
+ * Description: This function copies memory areas.
  * Return: a pointer to the memory area
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
-	s = malloc(sizeof(b));
-
-	if (s == NULL)
-		return (NULL);
 
 	for (i = 0; i < n; i++)
 	{
 		*(s + i) = b;
 	}
-	free(s);
 	return (s);
 }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
+#include <stdlib.h>
 
 char *_memset(char *s, char b, unsigned int n);
 
