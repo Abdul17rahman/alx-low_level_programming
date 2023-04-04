dc8ba39b86f2644a579e52abbddd2c69e1e69c00
Abdul Rahman <abdulnsamba@gmail.com>
2023-04-04 16:07:50 +0300

    strpk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Searches a string
+ * @s: Pointer to a string
+ * @accept: Byte to be searched
+ *
+ * Description: This function returns a pointer to string
+ * Return: a pointer to the first occurance
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	int len = strlen(s);
+	j = 0;
+
+	for (i = 0; i < len; i++)
+	{
+		if (*(accept + j) == *(s + i))
+		{
+			j++;
+			return (&s[i]);
+		}
+	}
+	return (NULL);
+}
