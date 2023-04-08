d9dcd8095b4861ea5d852dd82678202dab0be2c9
Abdul Rahman <abdulnsamba@gmail.com>
2023-04-08 16:45:00 +0300

    indexed

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * _atoi - checks
+ * @s: c is the character
+ *
+ * Description: This function check
+ * Return: interger
+ */
+
 int _atoi(char *s)
 {
 	return (atoi(s));
--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,5 +1,15 @@
 #include "main.h"
 
+/**
+ * _strncpy - copies a string
+ * @dest: destination string
+ * @src: Source string
+ * @n: byte size
+ *
+ * Description: This function copies a string
+ * Return: a pointer to the destination
+ */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	return (strncpy(dest, src, n));
