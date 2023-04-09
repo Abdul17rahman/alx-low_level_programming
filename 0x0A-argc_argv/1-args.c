3d3c05ef5b15c43bcd62573c8b3734a12e370a62
Abdul Rahman <abdulnsamba@gmail.com>
2023-04-09 12:35:38 +0300

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - prints the no. of arguments
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Description: This function prints its name from the command line
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%i\n", argc - 1);
+	return (0);
+}
