69ea10bba6a57080dcc102cb5720a855931db2fc
Abdul Rahman <abdulnsamba@gmail.com>
2023-04-09 17:02:23 +0300

    name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -11,13 +11,8 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
 	(void) argc;
 
-	for (i = 0; i < strlen(argv[0]); i++)
-	{
-		_putchar(argv[0][i]);
-	}
-	_putchar('\n');
+	printf("%s\n", argv[0]);
 	return (0);
 }
