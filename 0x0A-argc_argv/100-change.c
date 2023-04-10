dca5278fd23fc2fb228b58c7bdcfc71d6a25a2e8
Abdul Rahman <abdulnsamba@gmail.com>
2023-04-10 16:41:58 +0300

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,44 +11,39 @@
 
 int main(int argc, char *argv[])
 {
-	int cents, bal;
-
-	bal = 0;
+	int cents, bal = 0;
 
 	if ((argc == 2))
 	{
 		cents = atoi(argv[1]);
 		if (cents < 1)
-			printf("0\n");
-		else
+			bal = 0;
+		while (cents >= 25)
 		{
-			while (cents >= 25)
-			{
-				cents -= 25;
-				bal++;
-			}
-			while (cents >= 10 && cents < 25)
-			{
-				cents -= 10;
-				bal++;
-			}
-			while (cents >= 5 && cents < 10)
-			{
-				cents -= 5;
-				bal++;
-			}
-			while (cents >= 2 && cents < 5)
-			{
-				cents -= 2;
-				bal++;
-			}
-			while (cents == 1)
-			{
-				cents -= 1;
-				bal++;
-			}
-			printf("%d\n", bal);
+			cents -= 25;
+			bal++;
 		}
+		while (cents >= 10 && cents < 25)
+		{
+			cents -= 10;
+			bal++;
+		}
+		while (cents >= 5 && cents < 10)
+		{
+			cents -= 5;
+			bal++;
+		}
+		while (cents >= 2 && cents < 5)
+		{
+			cents -= 2;
+			bal++;
+		}
+		while (cents == 1)
+		{
+			cents -= 1;
+			bal++;
+		}
+		printf("%d\n", bal);
 	}
 	else
 	{
