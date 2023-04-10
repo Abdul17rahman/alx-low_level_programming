f842bd933310091f9b8d44cb4f0ba4efc03318c6
Abdul Rahman <abdulnsamba@gmail.com>
2023-04-10 16:24:47 +0300

    Change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/**
+ * main - prints the number of coins
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Description: This function calculates the coins change
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	int cents, bal;
+
+	bal = 0;
+
+	if ((argc == 2))
+	{
+		cents = atoi(argv[1]);
+		if (cents < 1)
+			printf("0\n");
+		else
+		{
+			while (cents >= 25)
+			{
+				cents -= 25;
+				bal++;
+			}
+			while (cents >= 10 && cents < 25)
+			{
+				cents -= 10;
+				bal++;
+			}
+			while (cents >= 5 && cents < 10)
+			{
+				cents -= 5;
+				bal++;
+			}
+			while (cents >= 2 && cents < 5)
+			{
+				cents -= 2;
+				bal++;
+			}
+			while (cents == 1)
+			{
+				cents -= 1;
+				bal++;
+			}
+			printf("%d\n", bal);
+		}
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
