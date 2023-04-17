674c95a5404ca5c0a9acbdd08c525c501b04b18f
Abdul Rahman <abdulnsamba@gmail.com>
2023-04-17 16:18:19 +0300

    Updated dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,12 +12,12 @@
  *
  * Description: This struct defines a custom dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}, dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
