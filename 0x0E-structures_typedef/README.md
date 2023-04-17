4fe053d1f44020fa5b683cb4adfbf1ae60bdb104
Abdul Rahman <abdulnsamba@gmail.com>
2023-04-17 12:47:53 +0300

    dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,5 @@
+Structures in C.
+
+A structure is a user defined type with a collection of different members/data types.
+
+They are similar to classes in other programming langauages.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Defines a dog
+ * @name: name of the dog char*
+ * @age: Age of the dog float
+ * @owner: name of the owner char*
+ *
+ * Description: This struct defines a custom dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
