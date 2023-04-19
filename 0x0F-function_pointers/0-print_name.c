226ac9c2018bbf65b08f1c70cc7c8e4f9ec986f6
Abdul Rahman <abdulnsamba@gmail.com>
2023-04-19 16:48:38 +0300

    print

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints the name.
+ * @name: the name to be printed
+ * @f: Pointer to a function
+ *
+ * Description: Uses function pointer to locate the function
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,3 @@
+Function Pointers.
+
+These are pointers to functions instead of variables, they point to addresses of a block of code in memory.
