5da520785cdabac1bc2c787b0745dc43a2c5652c
Abdul Rahman <abdulnsamba@gmail.com>
2023-05-03 00:15:56 +0300

    print

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * print_list - Prints the elements of a list.
+ * @h: pointer to a list
+ *
+ * Description: Uses a loop to traverse through a list
+ * Return: size_t, the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int el = 0;
+
+	const list_t *link;
+
+	link = h;
+
+	if (link->str == NULL)
+	{
+		printf("[0] (nil)\n");
+		return (0);
+	}
+
+	while (link)
+	{
+		printf("[%i] %s\n", link->len, link->str);
+		link = link->next;
+		el++;
+	}
+	return (el);
+}
