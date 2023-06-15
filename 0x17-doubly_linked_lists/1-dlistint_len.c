8fe0afa5a80be5f81357e28eb4dc718bb5e76250
Abdul Rahman <abdulnsamba@gmail.com>
2023-06-15 10:28:51 +0300

    len

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,21 +1,20 @@
 #include "lists.h"
 
 /**
- * print_dlistint - Prints the elements of a list.
+ * dlistint_len - counts the elements of a list.
  * @h: pointer to a list
  *
  * Description: Uses a loop to traverse through a list
  * Return: size_t, the number of nodes
  */
 
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	unsigned int el = 0;
 	const dlistint_t *temp = h;
 
 	while (temp != NULL)
 	{
-		printf("%d\n", temp->n);
 		temp = temp->next;
 		el++;
 	}
