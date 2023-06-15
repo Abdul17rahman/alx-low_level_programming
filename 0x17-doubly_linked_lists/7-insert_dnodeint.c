bf381256b51f3b522266dc8e6da2374635f66c7d
Abdul Rahman <abdulnsamba@gmail.com>
2023-06-15 13:40:48 +0300

    insert up

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,6 +16,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i;
 
 	i = 1;
+	if (idx == 0)
+		new_node = add_dnodeint(h, n);
 	new_node = malloc(sizeof(dlistint_t));
 
 	if (new_node == NULL)
