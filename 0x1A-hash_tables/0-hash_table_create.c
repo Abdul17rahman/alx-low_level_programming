9b9d133e1a3e3eb48932ae21304a32404265630b
Abdul Rahman <abdulnsamba@gmail.com>
2023-07-07 16:31:46 +0300

    table-

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -21,6 +21,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	table_nodes = malloc(sizeof(hash_node_t) * size);
+	new_table->size = size;
+	new_table->array = table_nodes;
 	if (table_nodes == NULL)
 	{
 		free(new_table);
@@ -31,7 +33,5 @@ hash_table_t *hash_table_create(unsigned long int size)
 		table_nodes[i] = NULL;
 		i++;
 	}
-	new_table->size = size;
-	new_table->array = table_nodes;
 	return (new_table);
 }
